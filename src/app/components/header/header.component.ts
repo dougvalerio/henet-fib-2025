23d2123863ac824381c9fa59a20978627ec146a2
Douglas <douglas.br80@hotmail.com>
2025-08-20 10:40:25 -0300

    Criado popup de senha

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   isMenuOpen = false;
-  navigationSubscription;
+  isPopupOpen = false;
+  passwordInput = '';
+  isPasswordIncorrect = false;
+  targetRoute = '';
+  private readonly correctPassword = 'h3n3t';
+  navigationSubscription: any;
 
   constructor(private router: Router) {
     this.navigationSubscription = this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe(() => {
         this.isMenuOpen = false;
+        this.closePasswordPopup();
       });
   }
 
@@ -28,6 +36,29 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   navigateToHome() {
     this.router.navigate(['/home']);
+  }
+
+  openPasswordPopup(route: string) {
+    this.targetRoute = route;
+    this.isPopupOpen = true;
+    this.passwordInput = '';
+    this.isPasswordIncorrect = false;
+  }
+
+  closePasswordPopup() {
+    this.isPopupOpen = false;
+    this.passwordInput = '';
+    this.isPasswordIncorrect = false;
+  }
+
+  submitPassword() {
+    if (this.passwordInput === this.correctPassword) {
+      this.router.navigate([this.targetRoute]);
+      this.closePasswordPopup();
+    } else {
+      this.isPasswordIncorrect = true;
+      this.passwordInput = '';
+    }
   }
 
   ngOnInit() {}
